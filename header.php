d72b9a29e58796ef3ee16c06fc6841392a53d15e
Harm Putman <harm@high-five.dev>
2022-01-19 23:30:41 +0100

    initial commit

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<!--[if lt IE 9]> <html class="no-js lte-ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php wp_title( '&#45;', true ,'right' ); ?><?php bloginfo( 'name' ); ?></title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <?php wp_head(); ?>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<section class="section">
+    <div class="container">
+        <h1 class="title">Hello Universe!</h1>
+        <p class="subtitle">HyperDrive Base theme is built on <strong>Bulma</strong>!</p>
+    </div>
+</section>
+
+<?php get_footer(); ?>
