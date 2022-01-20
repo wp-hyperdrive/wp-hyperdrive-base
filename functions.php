df5923756380b35dedb068fb8164b2ce8c3be308
Harm Putman <harm@high-five.dev>
2022-01-20 21:38:35 +0100

    Testing admin pages

--- functions.php
+++ functions.php
@@ -1,7 +1,26 @@
 <?php
 
+use Hyperdrive\AdminPages\Models\AdminPage;
 use Hyperdrive\Core\Bootstrap;
 
 defined('ABSPATH') or die('That\'s not how the Force works!');
 
 Bootstrap::load();
+
+add_filter(
+    'wp_hyperdrive_admin_pages',
+    function($pages) {
+        $page = new AdminPage();
+
+        $page->pageTitle = 'Test';
+        $page->menuTitle = 'Menu Title';
+        $page->capability = 'manage_options';
+        $page->menuSlug  = 'menu-slug-test';
+        $page->callback = function() { echo 'TEST'; };
+        $page->iconUrl = 'dashicons-dashboard';
+        $page->position = 59;
+
+        $pages[] = $page;
+        return $pages;
+    }
+);
