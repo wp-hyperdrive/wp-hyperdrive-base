c08c849fbe8df04ceb32933c4cd64f5a07b27f13
Harm Putman <harm@high-five.dev>
2022-01-21 10:35:24 +0100

    🚧

--- functions.php
+++ functions.php
@@ -1,28 +1,30 @@
 <?php
 
-use Hyperdrive\AdminPages\Models\AdminPage;
 use Hyperdrive\AdminPages\Bootstrap as AdminPages;
-use Hyperdrive\Core\Bootstrap as Core;
+use Hyperdrive\AdminPages\Models\AdminPage;
+use Hyperdrive\Core\Bootstrap;
 
-defined('ABSPATH') or die('That\'s not how the Force works!');
+defined('ABSPATH') || exit('That\'s not how the Force works!');
 
-Core::load();
-AdminPages::load();
+Bootstrap::load([
+    AdminPages::class,
+]);
 
 add_filter(
     'wp_hyperdrive_admin_pages',
-    function($pages) {
+    function ($pages) {
         $page = new AdminPage();
 
         $page->pageTitle = 'Test';
         $page->menuTitle = 'Menu Title';
         $page->capability = 'manage_options';
-        $page->menuSlug  = 'menu-slug-test';
-        $page->callback = function() { echo 'TEST'; };
+        $page->menuSlug = 'menu-slug-test';
+        $page->callback = function () { echo 'TEST'; };
         $page->iconUrl = 'dashicons-dashboard';
         $page->position = 59;
 
         $pages[] = $page;
+
         return $pages;
     }
 );
